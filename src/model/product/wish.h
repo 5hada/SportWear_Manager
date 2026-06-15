841224639bc7ff014695017f5d92627bc54687e2
5hada <81249902+5hada@users.noreply.github.com>
2026-06-16 00:23:43 +0900

    feat: wish model
    add getter, setter

--- src/model/product/wish.h
+++ src/model/product/wish.h
@@ -1,10 +1,15 @@
 
 
-
 class Wish{
     int userId;
     int productId;
 
 public:
     Wish(int userId, int productId): userId(userId), productId(productId) {}
+
+    int getUserId() const {return userId;}
+    int getProductId() const {return productId;}
+
+    void setUserId(int userId) {this->userId = userId;}
+    void setProductId(int productId) {this->productId = productId;}
 };
